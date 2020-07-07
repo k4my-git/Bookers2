eafe050ea1000b905e90d9e7ea2406477663b806
k4my-git <kambara0229@icloud.com>
2020-07-08 03:04:36 +0900

    web_kadai_2

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e0acbd9014a5bf44fdbf904996ccc0a99651712fa65e9b3bf13d8a9b686a25bcc4d7c7baa43e3b315534d519e8263c719053a065f1adc16f868fead678319900'
